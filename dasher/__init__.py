0e40766cbe6568f3c4a7be072618a0d9f92fb859
Miranda Chao <miranda_chao@brown.edu>
2015-02-08 09:22:04 -0500

    lol

--- dasher/__init__.py
+++ dasher/__init__.py
@@ -4,6 +4,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/dasher.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Users\\Miranda\Desktop\\dasher.db'
 
 db = SQLAlchemy(app)
 
