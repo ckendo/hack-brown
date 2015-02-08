d99f58a26c3aa814085b7297dcd65d1f9cd85be9
Tala Huhe <tala@fb.com>
2015-02-07 18:27:32 -0800

    Migrate more stuff over, delete old cruft

--- dasher/models.py
+++ dasher/models.py
@@ -1,5 +1,11 @@
 from dasher import db
 
+interest_identifier = db.Table(
+    'interest_identifier',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('interest_id', db.Integer, db.ForeignKey('interest.id')),
+)
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=False)
@@ -9,6 +15,7 @@ class User(db.Model):
     gender = db.Column(db.String(10), unique=False)
     language = db.Column(db.String(120), unique=False)
     description = db.Column(db.String(120), unique=False)
+    interests = db.relationship("Interest", secondary=interest_identifier)
 
     def __init__(self, name, image_url, age, state, gender, language, description):
         self.name = name
@@ -44,6 +51,7 @@ db.drop_all()
 db.create_all()
 
 prepopulated_users = [
+    User('Tala Huhe', '', 24, 'CA', 'male', 'English', 'I like potatoes'),
     User('Kenji Endo', '', 18, 'DE', 'male', 'English', 'I like kayaking'),
     User('Michael Markell', '', 19, 'DE', 'male', 'English', 'I like underwater basket weaving ;))'),
     User('Miranda Chao', '', 19, 'NY', 'female', 'English', 'I sleep a lot'),
@@ -53,6 +61,7 @@ prepopulated_users = [
 ]
 
 prepopulated_interests = [
+    Interest('Sleeping', '', 'Really really really fun outdoor activity underwater that\'s quite fun'),
     Interest('Underwater Basket Weaving', '', 'Really fun outdoor activity underwater that\'s quite fun'),
     Interest('Reindeer Watching', '', 'Like bird watching, but reindeer'),
     Interest('Kayaking', '', 'Like underwater basket weaving, but more adventurous'),
@@ -64,4 +73,17 @@ prepopulated_interests = [
 
 [db.session.add(model) for model in prepopulated_users]
 [db.session.add(model) for model in prepopulated_interests]
+
+prepopulated_users_to_interests_map = {
+    1: [6, 1, 4],
+    2: [5, 2, 1],
+    3: [4, 5, 6],
+    4: [7, 4],
+    5: [4, 7],
+    6: [6, 2]
+}
+
+for user_id, interest_ids in prepopulated_users_to_interests_map.iteritems():
+    [prepopulated_users[user_id].interests.append(prepopulated_interests[id]) for id in interest_ids]
+
 db.session.commit()
